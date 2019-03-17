5384f53987a267f3741210fb55ecb516e1dfae63
RiverXu <sdxujianghe@126.com>
2019-03-17 21:49:24 +0800

    Commit twice

--- src/main/java/nju/riverxu/ds/model/item/ItemCantUpgradeInfo.java
+++ src/main/java/nju/riverxu/ds/model/item/ItemCantUpgradeInfo.java
@@ -1,4 +1,11 @@
 package nju.riverxu.ds.model.item;
 
-public class ItemCantUpgradeInfo {
+public class ItemCantUpgradeInfo extends ItemUpgradeInfo {
+    private static ItemUpgradeInfo instance = new ItemCantUpgradeInfo();
+    public static ItemUpgradeInfo getInstance() {
+        return instance;
+    }
+    private ItemCantUpgradeInfo() {
+        super(false, null, 0);
+    }
 }
--- src/main/java/nju/riverxu/ds/model/item/ItemUpgradeInfo.java
+++ src/main/java/nju/riverxu/ds/model/item/ItemUpgradeInfo.java
@@ -1,4 +1,34 @@
 package nju.riverxu.ds.model.item;
 
 public class ItemUpgradeInfo {
+    boolean upgradable;
+    Item item;
+    int neededSoul;
+
+    public ItemUpgradeInfo(boolean upgradable, Item item, int neededSoul) {
+        this.upgradable = upgradable;
+        this.item = item;
+        this.neededSoul = neededSoul;
+    }
+
+    public boolean isUpgradable() {
+        return upgradable;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getNeededSoul() {
+        return neededSoul;
+    }
+
+    @Override
+    public String toString() {
+        return "升级项：{" +
+                "可升级 =" + upgradable +
+                ", 物品 =" + item.getName() +
+                ", 所需灵魂 =" + neededSoul +
+                '}';
+    }
 }
